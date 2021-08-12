66742e701bcf9eaf102201bb3c7160ba6309e501
john <t69685186@gmail.com>
2021-08-12 22:19:17 +0800

    表單新增刪除功能

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,31 +1,63 @@
 class NotesController < ApplicationController
-    def index
-      @notes = Note.all
-        
-    end
-   
-    def new
-      @note = Note.new
-    end
-   
-    def show
-      @note = Note.find(params[:d])
-    end
-   
-    def create
-      # title = params[:title]
-      # content = params[:content]
-      # note = Note.new(params[:note])
-      clean_note = params.require(:note).permit(:title,:content)
-      @note = Note.new(clean_note)
 
-      if @note.save
-        redirect_to "/notes"
-      else
-        #待處理
-        # redirect_to "/notes/new"
-        render :new
-        
-      end
+  before_action :find_note, only:[:show, :edit, :update, :destroy]
+
+
+  def index
+    # @notes = Note.all
+    @notes = Note.order(id: :desc)
+
+  end
+  
+  def edit
+  end
+  
+  def update
+    # title = params[:title]
+    # content = params[:content]
+    # note = Note.new(params[:note])
+
+    if @note.update(note_params)
+      redirect_to "/notes"
+    else
+      #待處理
+      # redirect_to "/notes/new"
+      render :edit
+      
     end
+  end
+
+  def new
+    @note = Note.new
+  end
+  
+  def show
+  end
+  
+  def create
+    # title = params[:title]
+    # content = params[:content]
+    # note = Note.new(params[:note])
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to "/notes"
+    else
+      #待處理
+      # redirect_to "/notes/new"
+      render :new
+    end
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to "/notes"
+  end
+  private
+  def note_params
+    params.require(:note).permit(:title,:content)
+  end
+  def find_note
+    @note = Note.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_08_10_022149) do
 
+  create_table "notees", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -22,6 +29,14 @@ ActiveRecord::Schema.define(version: 2021_08_10_022149) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "accout"
+    t.string "password"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
